6b4b386eff54ab71b3847181280c288e680f1ad1
Timothy Barreras <lchorns@outlook.com>
2026-04-02 01:30:02 +0000

    Add HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Timothy Barreras.");
+        System.out.println("I'm taking Java 1 and this is my first program!");
+    }
+}
